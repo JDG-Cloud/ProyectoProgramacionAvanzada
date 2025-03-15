95498c443460cf10bbe0f6f08db0d546c58b9c9f
Camilo Ramos <cristian.ramos@celuweb.com>
2025-03-14 20:58:00 -0500

    prueba 5

--- src/main/java/co/edu/uniquindio/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/ProyectoApplication.java
@@ -10,6 +10,6 @@ public class ProyectoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectoApplication.class, args);
     }
-    /*hola mundo 4*/
+    /*hola mundo 5*/
 
 }
