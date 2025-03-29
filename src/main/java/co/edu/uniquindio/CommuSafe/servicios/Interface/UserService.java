745e3d123ced21b2047a2c96cf4f0845f71d707a
JDGPuni <juand.gutierrezp@uqvirtual.edu.co>
2025-03-28 21:48:47 -0500

    Edicion de  documentos y service e implementation nuevos

--- src/main/java/co/edu/uniquindio/CommuSafe/servicios/Interface/UserService.java
+++ src/main/java/co/edu/uniquindio/CommuSafe/servicios/Interface/UserService.java
@@ -2,18 +2,21 @@ package co.edu.uniquindio.CommuSafe.servicios.Interface;
 
 import co.edu.uniquindio.CommuSafe.dto.usuarios.CrearUsuarioDTO;
 import co.edu.uniquindio.CommuSafe.dto.usuarios.EditarUsuarioDTO;
+import co.edu.uniquindio.CommuSafe.dto.usuarios.UsuarioDTO;
 import co.edu.uniquindio.CommuSafe.modelo.User;
+import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
-public interface UserService extends MongoRepository<User, ObjectId> {
+public interface UserService  {
 //tiene los mismos metodos de la api
     String createUser(CrearUsuarioDTO createUserDTO) throws Exception;
     String editUser(EditarUsuarioDTO editUserDTO);
     String deleteUser(String deleteUserId);
+    String getUser(String id);
 
-    //String createReport(CreateReportDTO createReportDTO);
 }
